d9e6ce633bc495a2cb963bc7c9c65c4d9fd62502
Parsa Hassani <33244417+parsacubesat@users.noreply.github.com>
2020-05-11 18:12:55 -0700

    Initial

---
+++ Needle Shooter/Assets/CircleSpinner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CircleSpinner : MonoBehaviour
+{
+    public float turnSpeed = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        turnSpeed = UnityEngine.Random.Range(1, 7);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, turnSpeed, Space.Self);
+    }
+}
